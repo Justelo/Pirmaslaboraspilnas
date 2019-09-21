ebf85b38b453e5c4a5187b259d3a54de08d784c9
Justelo <55538339+Justelo@users.noreply.github.com>
2019-09-21 07:17:42 -0700

    Idetas mygtukas keiciantis spalva

--- app/src/main/java/com/example/a2/MainActivity.java
+++ app/src/main/java/com/example/a2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button changeTextButton;
+    Button changeColorButton;
     TextView message;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeColorButton = (Button)findViewById(R.id.bChangeColor);
+        changeColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message.setTextColor(Color.BLUE);
+            }
+        });
         message = (TextView)findViewById(R.id.tvMessage);
         changeTextButton = (Button)findViewById(R.id.bChangeText);
         changeTextButton.setOnClickListener(new View.OnClickListener(){
